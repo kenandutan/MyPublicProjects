6d5177ed5d2e2c23604df7a51537259ce8afbc8e
kenandutan <143758903+kenandutan@users.noreply.github.com>
2023-09-20 18:19:04 -0700

    Update shibabot_v1.py

--- shibabot_v1.py
+++ shibabot_v1.py
@@ -5,7 +5,7 @@ import random
 class ShibaBot:
 
   # keywords for exiting the interaction
-  exit_commands = ("quit", "exit", "goodbye", "bye", "later", "see ya")
+  exit_commands = ("goodbye", "bye", "later", "see ya", "see you")
   # random shiba actions
   shiba_actions = (
         "*The Shiba scratches their ear* ",
@@ -13,16 +13,18 @@ class ShibaBot:
         "*The Shiba starts sniffing the ground* ",
         "*The Shiba starts licking their paw* ",
         "*The Shiba stares at you* ",
-        "*The Shiba looks away from you* ",
-        "*The Shiba stretches in a downward dog position* "
+        "*The Shiba stretches in a downward dog position* ",
+        "*The Shiba wags his tail* ",
+        "*The Shiba jumps up on you* ",
+        "AH-WOOOOOO!!"
     )
-
+ 
   def __init__(self):
-    self.commands = {'speak_intent': r'.*\s*speak.*', 'sit_intent': r'.*\s*sit.*', 'high_five_intent': r'.*\s*high*.*five*', 'down_intent': r'.*\s*down.*'}
+    self.commands = {'speak_intent': r'.*\s*speak.*', 'sit_intent': r'.*\s*sit.*', 'high_five_intent': r'.*\s*high*.*five*', 'down_intent': r'.*\s*down.*', 'paw_intent': r'.*\s*paw.*', 'spin_intent': r'.*\s*snap.*finger*', 'bang_intent': r'.*\s*bang.*', 'follow_intent': r'.*let.*go*', 'yip_intent': r'.*yip*.*yip*', 'pet_intent': r'.*pet*.*', 'scratch_intent': '.*scratch*.*'}
 
   # when you encounter the Shiba
   def meet(self):
-    print("*You see a dog. You quickly realize it's a Shiba Inu and walk up to it.* ")
+    print("*You see a dog. You notice it's a Shiba Inu and walk up to it.* ")
 
     self.shiba_interaction()
 
@@ -30,7 +32,7 @@ class ShibaBot:
   def make_exit(self, reply):
     for exit_command in self.exit_commands:
       if exit_command in reply:
-        print("*You walk past the Shiba and go along with your day* ")
+        print("*You walk away from the Shiba* ")
         return True
 
   # continuously interacting with Shiba
@@ -52,33 +54,81 @@ class ShibaBot:
         return self.high_five_intent()
       elif found_match and intent == 'down_intent':
         return self.down_intent()
-      else:
-        return self.no_match_intent()
+      elif found_match and intent == 'paw_intent':
+        return self.paw_intent()
+      elif found_match and intent == 'spin_intent':
+        return self.spin_intent()
+      elif found_match and intent == 'bang_intent':
+        return self.bang_intent()
+      elif found_match and intent == 'follow_intent':
+        return self.follow_intent()
+      elif found_match and intent == 'pet_intent':
+        return self.pet_intent()
+      elif found_match and intent == 'yip_intent':
+        return self.yip_intent()
+      elif found_match and intent == 'scratch_intent':
+        return self.scratch_intent()
 
+    return self.no_match_intent()
 
   # speak command
   def speak_intent(self):
-    responses = ("BARK! ", "*The Shiba stares at you* ", "WOOF! ")
+    responses = ("BARK! ", "*The Shiba does nothing* ", "WOOF! ")
     return random.choice(responses)
 
   # sit command
   def sit_intent(self):
-    responses = ("*The Shiba sits down* ", "*The Shiba lays down* ", "*The Shiba does nothing* ", "*The Shiba stares at you* ")
+    responses = ("*The Shiba sits down* ", "*The Shiba does nothing* ")
     return random.choice(responses)
 
   # high five command
   def high_five_intent(self):
-    responses = ("*The Shiba gives you a high* ", "*The Shiba does nothing* ", "*The Shiba stares at you* ")
+    responses = ("*The Shiba gives you a high five* ", "*The Shiba does nothing* ")
     return random.choice(responses)
 
   # down command
   def down_intent(self):
-    responses = ("*The Shiba lays down* ", "*The Shiba sits down* ", "*The Shiba does nothing* ", "*The Shiba stares at you* ")
+    responses = ("*The Shiba lays down* ", "*The Shiba does nothing* ")
     return random.choice(responses)
+
+  # paw command
+  def paw_intent(self):
+    responses = ("*The Shiba touches your hand with their paw* ", "*The Shiba does nothing* ")
+    return random.choice(responses)
+
+  # spin command
+  def spin_intent(self):
+    responses = ("*The Shiba does a spin* ")
+    return responses
+
+  # play dead command
+  def bang_intent(self):
+    responses = ("*The Shiba falls down with their feet in the air* ")
+    return responses
+
+  # follow command
+  def follow_intent(self):
+    responses = ("*You start walking and the Shiba follows you* ")
+    return responses
+
+    # yip command
+  def yip_intent(self):
+    responses = ("*You start walking and the Shiba quickly follows you* ")
+    return responses
+
+  # pet command
+  def pet_intent(self):
+    responses = ("*The Shiba wags their tail* ", "*The Shiba grins* ")
+    return random.choice(responses)
+
+    # scratch command
+  def scratch_intent(self):
+    responses = ("*The Shiba smiles with their toungue sticking out* ")
+    return responses
 
   # when the reply doesn't match any intent
   def no_match_intent(self):
-    responses = ("*The Shiba does nothing* ", "*The Shiba stares at you* ", "BARK! ", "*The Shiba tilts its head, confused* ", "*The Shiba whines* ", "*The Shiba starts panting* ", "*The Shiba looks away from you* ", "*The Shiba yawns* ")
+    responses = ("*The Shiba stares at you* ", "*The Shiba tilts its head, confused* ", "*The Shiba whines* ", "*The Shiba starts panting* ", "*The Shiba looks away from you* ", "*The Shiba yawns* ", "*The Shiba lays down and starts sleeping* ")
     return random.choice(responses)
 
 # calling the ShibaBot
